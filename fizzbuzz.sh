2cec2c0d816cbdd3aeda25e45d6d5e0f4483e9f4
James Aluoch <49788423+AluochT@users.noreply.github.com>
2022-03-31 11:15:26 -0500

    Add files via upload

--- fizzbuzz.sh
+++ fizzbuzz.sh
@@ -5,19 +5,19 @@
 
 for ((number = 0; number <= 20; number++))
 do
-   if (($number % 3 == 0))
+   if (($number % 3 == 0)) # check if number is divisible by 3
    then
    echo "fizz"
-   elif (($number % 5  == 0 ))
+   elif (($number % 5  == 0 )) # check if number is divisible by 5
    then 
      echo "buzz"
-   elif (($number % 3 == 0 &&  $number % 5 == 0 ))
+   elif (($number % 3 == 0 &&  $number % 5 == 0 )) # check if number is divisible  by 3 and 5
    then 
       echo  "fizzbuzz"
-   elif (($number % 3  != 0 &&  $number %  5  != 0 ))
+   elif (($number % 3  != 0 &&  $number %  5  != 0 )) # check if number is not divisible y 3 and 5 
    then
      echo "nothing"
-   elif (($number  < 1= 0 &&  $number  > 20 ))
+   elif (($number  < 1  || $number  > 21 )) # check if number or valued entered is out of range or invalid value
      then
       echo "Number enter is out of range or invalid value"
     else
